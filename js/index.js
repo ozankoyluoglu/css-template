918752c2b62261be76cafa4f26c6ee8b0a6ceec8
ozankoyluoglu <ozankoy@yahoo.com>
2017-03-28 21:50:12 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,48 +1,8 @@
-$(document).ready(function() {
-    // Configure/customize these variables.
-    var showChar = 100;  // How many characters are shown by default
-    var ellipsestext = "...";
-    var moretext = "Show more >";
-    var lesstext = "Show less";
-    
-
-    $('.more').each(function() {
-        var content = $(this).html();
- 
-        if(content.length > showChar) {
- 
-            var c = content.substr(0, showChar);
-            var h = content.substr(showChar, content.length - showChar);
- 
-            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
- 
-            $(this).html(html);
-        }
- 
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("p").hide();
     });
- 
-    $(".morelink").click(function(){
-        if($(this).hasClass("less")) {
-            $(this).removeClass("less");
-            $(this).html(moretext);
-        } else {
-            $(this).addClass("less");
-            $(this).html(lesstext);
-        }
-        $(this).parent().prev().toggle();
-        $(this).prev().toggle();
-        return false;
+    $("#show").click(function(){
+        $("p").show();
     });
-});
-
-
-
-$.fn.extend({
-  toggleText: function(a, b) {
-    if (this.html() == a) {
-      this.html(b)
-    } else {
-      this.html(a)
-    }
-  }
 });
